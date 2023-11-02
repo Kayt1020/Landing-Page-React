9247e88faa1801256b935dcbd7b3001970511830
Kayt1020 <143145652+Kayt1020@users.noreply.github.com>
2023-11-02 00:24:16 +0000

    step one

---
+++ landing page react/src/Navbar.jsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#mainPage">MainPage</a>
+          <a href="#pricing">Pricing</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
