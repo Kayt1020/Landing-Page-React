fd7b7f7df4bd66a87373d9a055fe735cc241101a
Kayt1020 <143145652+Kayt1020@users.noreply.github.com>
2023-11-02 00:55:30 +0000

    11.1

--- landing page react/src/App.jsx
+++ landing page react/src/App.jsx
@@ -1,26 +1,18 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
-
-import { Navbar } from "./Navbar.jsx"
-
-//function App() {
- // const [count, setCount] = useState(0)
-
-// <button onClick={() => setCount((count) => count + 1)}>
- // count is {count}
-//</button>
-
-import (Navbar) from "./components/Navbar"
+import { Navbar } from "./components/navigation/Navbar.jsx"
 
 export function App(){
   return (
     <>
-    <h1> LandingPageReact</h1>
+    <Navbar />
     </>
   )
 }
 
 
 export default App
+
+
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
---
+++ landing page react/src/components/navigation/Navbar.jsx
@@ -0,0 +1,13 @@
+import'./navbar.css'
+export function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#realestateprofessional">Real Estate Professional</a>
+          <a href="#thebecgroup"> The Bec Group</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
